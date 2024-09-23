15aeeb12db1f9f077abcc11eae328534f3442e54
HashLenox <hslenox0419@gmail.com>
2024-09-24 02:18:23 +0530

    Install breeze, and Livewire Hot Refresh 🔧

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+import { livewire_hot_reload } from 'virtual:livewire-hot-reload'
+livewire_hot_reload();
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
