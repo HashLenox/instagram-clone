e1cffa669ac3a93cf343fd52742888d148dc2af7
HashLenox <hslenox0419@gmail.com>
2024-10-01 00:29:44 +0530

    Loading more posts when scroll down ✨

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Post;
+use App\Models\User;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
@@ -11,6 +12,11 @@ class Home extends Component
 
     public $posts;
 
+    public $canLoadMore;
+    public $perPageIncrements=5;
+    public $perPage=10;
+
+
     #[On('closeModal')]
     function reverUrl()
     {
@@ -18,27 +24,61 @@ class Home extends Component
     }
 
 
-
-   #[On('post-created')]
-
-    function postCreated($id)
+    #[On('post-created')]
+    function postCreaed($id)
     {
-        $post= Post::find($id);
 
-        $this->posts=$this->posts->prepend($post);
+        $post = Post::find($id);
+
+        $this->posts = $this->posts->prepend($post);
     }
 
+
+
+    function loadMore()  {
+
+
+     //   dd('here');
+        if (!$this->canLoadMore) {
+
+            return null;
+        }
+
+
+        #increment page
+        $this->perPage += $this->perPageIncrements;
+
+        #load posts
+        $this->loadPosts();
+
+
+    }
+
+
+    #function to load posts
+
+    function loadPosts()  {
+
+        $this->posts = Post::with('comments.replies')
+        ->latest()
+        ->take($this->perPage)->get();
+
+        $this->canLoadMore= (count($this->posts)>= $this->perPage);
+
+    }
 
 
     function mount()
     {
-        $this->posts = Post::with('comments')->latest()->get();
+
+        $this->loadPosts();
 
     }
-
 
     public function render()
     {
         return view('livewire.home');
     }
+
+
 }
